a6ceb4b23d3599b04a81ee884dc4aa6a05523dc8
rayhuba <huba855@gmail.com>
2024-03-11 18:12:43 +0300

    My first commit

---
+++ STATEMENTS.dart
@@ -0,0 +1,12 @@
+// statement 
+void main() {
+  var age = 18;
+  if (age > 18){
+    print("Mariam is a voter in Kenya");
+  }else if(age== 18) {
+    print("Mariam has just become eligible to vote in kenya");
+  }
+  else{
+    print("Mariam is still young to vote");
+  }
+}
---
+++ switchstatements.dart
@@ -0,0 +1,18 @@
+void main() {
+  int i =3;
+  switch(i) {
+    case 1:
+    print("The value is 1");
+    break;
+    case 2:
+    print("The value is 2");
+    break;
+    case 3:
+    print("The value is 3");
+    break;
+    default:
+    print("The value is out of range");
+    break;
+
+  }
+}
